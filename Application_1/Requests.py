7bc9b51f8350d486f7e752d7634bc3788a4ee5b8
iirepin <iirepnin@gmail.com>
2024-12-14 22:10:22 +0300

    Путь URL

--- Application_1/Requests.py
+++ Application_1/Requests.py
@@ -1,4 +1,4 @@
 import requests # requests - отправляет запросы и обрабатывает ответы
-r = requests.get("http://localhost:8000/hi") # отправляем запрос на сервер
+r = requests.get("http://localhost:8000/hi/Mom") # отправляем запрос на сервер
 r.json() # ответ преобразуем в формат json
 
